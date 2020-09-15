bef36e070f4df08e637cf71b4b4ae362dc09e31f
sylo <desocial@protonmail.com>
2020-09-15 22:20:34 +0100

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>This is a search engine app</h1>
-
-      {/* Home {the one with the search on} */}
-      <Home />
-
-      {/* Search Page {Results page} */}
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
